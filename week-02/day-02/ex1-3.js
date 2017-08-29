05a96d3fe3a7c072c5b9c11be03f023908f2c80e
Chase <chase.wang@hotmail.co.uk>
2017-08-29 16:26:52 +0800

    29082017

---
+++ week-02/day-02/ex1-3.js
@@ -0,0 +1,41 @@
+'use strict';
+
+console.log('Hello world!');
+
+console.log('Hello Chase!');
+console.log('Hello Jerry!');
+console.log('Hello Alex!');
+
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Humpty Dumpty had a great fall.');
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Couldn\'t put Humpty together again.\n');
+
+console.log(1);
+console.log(123);
+console.log(2.3);
+console.log(0);
+
+console.log(12341234123412341234 + '\n');
+
+console.log(3.141592);
+console.log(Math.PI);
+console.log(0.0);
+console.log(5.);
+console.log(.5);
+
+console.log(1+1);
+console.log(3-2);
+console.log(3*5);
+console.log(6/2);
+console.log(5%3);
+
+console.log(true);
+console.log(!false);
+console.log(true&&false);
+
+console.log('apple');
+console.log('don\'t');
+console.log('"For God\'s sake!!"');
+console.log('tooth' + 'brush');
+console.log('baby' + '-siter');
---
+++ week-02/day-02/ex4-6.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// self-introduction
+console.log('My name: Chasssssse');
+console.log('Age: 12.4 yrs');
+console.log('Height: 2.08m\n');
+
+// operations of two numbers
+console.log(13+22);
+console.log(22-13);
+console.log(22*13);
+console.log(22/13);
+console.log(22%13);
+
+// coding hours
+console.log('coding hours: 6 * 5 * 17 = ' + 6*5*17 + ' hrs');
+console.log('Percentage of the coding hours: 510 / 52 * 17 = ' + 510/52/17*100 + '%');
+
+
