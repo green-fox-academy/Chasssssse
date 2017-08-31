9c43b780af77956817f4f3cb3d5627f0a39c378e
Chase <chase.wang@hotmail.co.uk>
2017-08-31 23:18:36 +0800

    fixed exercise-12

--- week-02/day-04/exercise-12/exercise-12.js
+++ week-02/day-04/exercise-12/exercise-12.js
@@ -24,3 +24,48 @@ var ingredients = [
 	{ 'soda': 100, 'needs_cooling': true }
 ]
 
+var length_c1 = 'Ingredient'.length;
+var length_c2 = 'Needs cooling'.length;
+var length_c3 = 'In stock'.length;
+var longest = length_c1;
+
+ingredients.forEach(function(element) {
+	for (let keys in element) {
+		keys.length > longest ? longest = keys.length : longest = longest;
+	}
+});
+
+function border(l1, l2, l3) {
+	console.log('+' + '-'.repeat(l1 + 2) + '+' + '-'.repeat(l2 + 2) + '+' + '-'.repeat(l3 + 2) + '+');
+}
+
+function tags(l1, l2, l3) {
+	console.log('| ' + 'Ingredient' + ' '.repeat(longest + 1 - l1 ) + '| Needs Cooling | In stock |' );
+}
+
+function content(l1, l2, l3) {
+	ingredients.forEach(function(element, index) {
+
+		for (let keys in element) {
+			if (keys != 'needs_cooling') {
+				var cool;
+				var num_in_stock;
+				element[keys] == 0 ? num_in_stock = '-' : num_in_stock = element[keys].toString();
+				element.needs_cooling 
+				? cool = '| Yes' + ' '.repeat(l2 -2) + '| ' + num_in_stock + ' '.repeat(l3 + 1 - num_in_stock.length) + '|' 
+				: cool = '| No' + ' '.repeat(l2 -1) + '| ' + num_in_stock + ' '.repeat(l3 + 1 - num_in_stock.length) + '|';
+				
+				console.log('| ' + keys + ' '.repeat(longest + 1 - keys.length) + cool);
+			}
+		}
+	})
+}
+
+
+border(longest, length_c2, length_c3);
+tags(length_c1, length_c2, length_c3);
+border(longest, length_c2, length_c3);
+content(longest, length_c2, length_c3);
+border(longest, length_c2, length_c3);
+
+
